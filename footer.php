9d240bfbe0b751e9b3af611dc425f3c28c3e8ae3
Sven Henze <m@suvuk.de>
2012-05-14 16:01:53 +0200

    Changes for Piwik-Tracking
    Signed-off-by: Sven Henze <m@suvuk.de>

--- footer.php
+++ footer.php
@@ -5,6 +5,17 @@
 </div><!--  #accessmobile -->	
 <div class="clear"></div>
 <?php wp_footer() ?>
-
+<!-- Piwik -->
+<script type="text/javascript">
+var pkBaseURL = (("https:" == document.location.protocol) ? "https://piwik.piraten-unterfranken.de/" : "http://piwik.piraten-unterfranken.de/");
+document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+</script><script type="text/javascript">
+try {
+var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 1);
+piwikTracker.trackPageView();
+piwikTracker.enableLinkTracking();
+} catch( err ) {}
+</script><noscript><p><img src="http://piwik.piraten-unterfranken.de/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
+<!-- End Piwik Tracking Code -->
 </body>
 </html>
